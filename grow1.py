f448b781f9cc401a0d63c1263242a892c9e54c64
Peter Berck <durian@users.noreply.github.com>
2020-03-28 15:58:31 +0100

    Add files via upload

---
+++ grow1.py
@@ -0,0 +1,88 @@
+import re
+import sys, os
+import argparse
+from datetime import datetime
+from datetime import timedelta
+#https://stackoverflow.com/questions/35066588/is-there-a-simple-way-to-increment-a-datetime-object-one-month-in-python
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import matplotlib as mpl
+mpl.use("Qt5Agg") #TkAgg crashes
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+from matplotlib.dates import MonthLocator, YearLocator, DateFormatter, WeekdayLocator
+
+fn = "COVID-19-geographic-disbtribution-worldwide.csv"
+df = pd.read_csv( fn, sep="," )
+#df['DateRep'] = df['DateRep'].apply(lambda x: dt.datetime.strptime(x,'%d/%m/%Y'))
+df.loc[:,'DateRep'] = pd.to_datetime(df['DateRep']) #, format='%d/%m/%Y')
+print( df )
+
+populations = { }
+populations["SE"] = 10.080176 # in 1000,000s 
+populations["NL"] = 17.123469
+populations["DK"] =  5.786051
+populations["NO"] =  5.408352
+populations["DE"] = 83.703211
+populations["BE"] = 11.574389
+populations["IR"] = 83.662908
+populations["IT"] = 60.488377
+populations["IS"] =  0.340596
+
+dfs = {}
+
+if False:
+    c = "EU" # Hardcoded EU
+    dfs[c] = df[ df["EU"] == "EU" ]
+    #dfs[c] = dfs[c][ dfs[c]["DateRep"] > "2020-02-12" ]
+    dfs[c].sort_values( by=['DateRep'], inplace=True )
+    #dfs[c].loc[:, 'Sum']     = dfs[c]['NewConfCases'].cumsum()
+    dfs[c].loc[:, 'Sum']     = dfs[c]['Cases'].cumsum()
+    dfs[c].loc[:, 'SumNorm'] = dfs[c]['Sum'] / 1000
+    print( dfs[c] )
+
+for c in ["SE", "NL", "NO", "DK"]:
+    if not c in populations:
+        populations[c] = 1000
+    dfs[c] = df[ df["GeoId"] == c ]
+    #dfs[c] = dfs[c][ dfs[c]["DateRep"] > "2020-02-12" ]
+    dfs[c].sort_values( by=['DateRep'], inplace=True )
+    #dfs[c].loc[:, 'Sum']     = dfs[c]['NewConfCases'].cumsum()
+    dfs[c].loc[:, 'Sum']     = dfs[c]['Cases'].cumsum()
+    dfs[c].loc[:, 'SumNorm'] = dfs[c]['Sum'] / populations[c]
+    print( dfs[c] )
+    print( dfs[c]["Sum"].values )
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
+for c in dfs:
+    print( c )
+    #ax.plot( df_SE["DateRep"], df_SE["NewConfCases"], label="SE new" )
+    ax.plot( dfs[c]["DateRep"], dfs[c]["SumNorm"], label=c )
+
+ax.xaxis.set_major_formatter( DateFormatter('%Y-%m-%d') )
+ax.xaxis.set_minor_locator( WeekdayLocator() )
+fig.autofmt_xdate()
+plt.tight_layout()
+ax.grid()
+ax.legend(fontsize='x-small', labelspacing=0.2, frameon=True)
+plt.show(block=True)
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
+for i, c in enumerate(["SE", "NL"]): #, "NO", "DK", "DE", "BE"]:
+    dfs[c] = dfs[c][ dfs[c]["DateRep"] >= "2020-03-01" ]
+    rects = ax.bar( dfs[c]["DateRep"] + i*pd.Timedelta('6 hours'),
+                    dfs[c]["Sum"],
+                    alpha=0.8,
+                    #width=0.5,
+                    width=pd.Timedelta('6 hours'), #24, #in "days"
+                    align="center",
+                    label=c,
+                    #color=colours[0],
+                    #edgecolor="black",
+    )
+fig.autofmt_xdate()
+plt.tight_layout()
+ax.grid()
+ax.legend(fontsize='x-small', labelspacing=0.2, frameon=True)
+plt.show(block=True)
---
+++ grow4.py
@@ -0,0 +1,204 @@
+import re
+import sys, os
+import argparse
+from datetime import datetime
+from datetime import timedelta
+#https://stackoverflow.com/questions/35066588/is-there-a-simple-way-to-increment-a-datetime-object-one-month-in-python
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import matplotlib as mpl
+mpl.use("Qt5Agg") #TkAgg crashes
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+from matplotlib.dates import MonthLocator, YearLocator, DateFormatter, WeekdayLocator
+import matplotlib.ticker as plticker
+import matplotlib.dates as mdates
+
+# Data:
+#   git clone https://github.com/CSSEGISandData/COVID-19.git
+
+cat20_colours = [
+    "#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c",
+    "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5",
+    "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", 
+    "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5",
+    #https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+    #"#e6194B", "#3cb44b", "#ffe119", "#4363d8", "#f58231",
+    #"#911eb4", "#42d4f4", "#f032e6", "#bfef45", "#fabebe",
+    "#469990", "#e6beff", "#9A6324", "#fffac8", "#800000",
+    "#aaffc3", "#808000", "#ffd8b1", "#000075", "#a9a9a9"                 
+]
+
+parser = argparse.ArgumentParser()
+parser.add_argument( '-a', "--absolute", action='store_true', default=False, help='Do not normalise' )
+parser.add_argument( '-A', "--all", action='store_true', default=False, help='All countries with populations' )
+parser.add_argument( '-c', '--countries', type=str, default="Sweden,Netherlands", help='Countries')
+parser.add_argument( '-f', '--function', type=str, default="power", help='Which function to fit (power, exponential)')
+parser.add_argument( '-g', '--graph', type=str, default="confirmed", help='Which data set (confirmed, deaths)')
+parser.add_argument( '-l', "--last_n", type=int, default=28, help='Last n days' ) 
+args = parser.parse_args()
+
+#fn = "./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+#fn = "./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+if args.graph[0] == "c":
+    fn="./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+else:
+    fn="./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+
+'''
+[pberck@margretetorp Python]$ grep Netherlands time_series_covid19_deaths_global.csv
+Province/State,Country/Region,Lat,Long,1/22/20, ....
+Aruba,Netherlands,12.5186,-70.0358,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+Curacao,Netherlands,12.1696,-68.99,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1
+Sint Maarten,Netherlands,18.0425,-63.0548,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+,Netherlands,52.1326,5.2913,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,3,3,4,5,5,10,12,20,24,43,58,76,106,136,179,213,276,356
+'''
+
+df = pd.read_csv( fn, sep="," )
+print( df )
+
+graphs     = {}
+last_n     = args.last_n
+population = {}
+#https://www.worldometers.info/world-population/norway-population/
+population["Sweden"]      =  10.080176 # in 1000,000s (seems common)
+population["Netherlands"] =  17.123469
+population["Denmark"]     =   5.786051
+population["Norway"]      =   5.408352
+population["Germany"]     =  83.703211
+population["Belgium"]     =  11.574389
+population["Iran"]        =  83.662908
+population["Italy"]       =  60.488377
+population["Iceland"]     =   0.340596
+population["Spain"]       =  46.749597
+population["US"]          = 330.488824
+
+#countries = ["Netherlands", "Sweden", "Denmark", "Norway", "Germany", "Belgium", "Iran", "Italy"]
+if args.all:
+    countries = population.keys()
+else:
+    countries = args.countries.split(",")
+
+title_str = "Normalised per 1e6 inhabitants"
+if args.absolute:
+    title_str = "Absolute numbers"
+    
+ymax = 0
+ymin = 0
+first_date = {}
+last_date  = {}
+for country in countries:
+    dfc=df[ (df["Country/Region"]==country) & (df["Province/State"].isnull())  ]
+    print( dfc )
+    cols = dfc.columns[-last_n:] # list with dates
+    try:
+        data = dfc.iloc[0,4:]
+        data = data[data>0] # What if 0 in the middle? 
+        print( "data\n", data )
+    except IndexError:
+        continue
+    if not args.absolute:
+        try:
+            pop = population[country]
+        except KeyError:
+            pop = 1
+    else:
+        pop = 1
+    date_index = [datetime.strptime(x, '%m/%d/%y') for x in data.index]
+    data.index = date_index
+    print( date_index )
+    graphs[country] = data / pop # normalise
+    ymax = max( ymax, max(graphs[country]) )
+    first_date[country] = date_index[0]
+    last_date[country]  = date_index[-1]
+    print( first_date, min(first_date.values()), max(last_date.values()) )
+
+xlabels = pd.date_range(start=min(first_date.values()), end=max(last_date.values()))
+xcount = list( range(0, 20) )
+print( xlabels )
+           
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
+
+#print( graphs )
+
+for i, g in enumerate(graphs):
+    print( g )
+    ax.plot( graphs[g], label=g, c=cat20_colours[i] )
+    #ax.plot( graphs[g].index, graphs[g], label=g, c=cat20_colours[i] )
+    ax.scatter( x=graphs[g].index, y=graphs[g].values, c=cat20_colours[i], alpha=0.4 )
+fig.autofmt_xdate()
+#ax.set_yscale("log")
+ax.set_title( title_str )
+ax.set_xlabel( "Data from https://github.com/CSSEGISandData/COVID-19" )
+plt.tight_layout()
+ax.grid(linestyle='-', axis="y", alpha=0.5)
+yrange = ymax - ymin
+if yrange > 1000:
+    yinc   = round(yrange // 10, -2)
+    yinc   = yinc
+elif yrange > 100:
+    yinc   = round(yrange // 10, -1)
+    yinc   = yinc
+elif yrange > 10:
+    yinc   = yrange // 10
+else:
+    yinc = 1
+print( yrange, yinc )
+loc = plticker.MultipleLocator(base=yinc) # this locator puts ticks at regular intervals
+ax.yaxis.set_major_locator(loc)
+days = mdates.DayLocator()
+ax.xaxis.set_major_locator(days)
+
+#ax.legend(fontsize='x-small', labelspacing=0.2, frameon=True)
+ax.legend(labelspacing=0.2, frameon=True)
+
+pngfile="covid.png"
+fig.savefig(pngfile, dpi=300)
+plt.show()
+#
+
+import scipy.optimize as sio
+
+def f(x, A, B):
+    if args.function[0] == "e":
+        return A + np.exp(B*x)
+    else:
+        return A * np.power(x, B) 
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
+
+x1 = range(0, len(xcount) + 1 ) # One extra day
+x1labels = [first_date_dt + pd.Timedelta('1 day')*d for d in x1 ]
+x1 = np.array(x1)
+
+for i, g in enumerate(graphs):
+    print( g )
+    x = np.array(xcount)
+    y = np.array(graphs[g])
+    print( x, y[0] )
+    coeffs, coeffs_cov = sio.curve_fit(f, x, y, p0=(0, 1), bounds=(0, +np.inf) )
+    print( coeffs )
+    print( coeffs_cov )
+    # original data
+    ax.scatter(xlabels, y, c=cat20_colours[i], label=g)
+    #ax.plot(xlabels, y, c=cat20_colours[i], linewidth=1, label=g) # labels/range from above
+    #
+    pred_y = f(x1, *coeffs)
+    print( y )
+    print( pred_y )
+    cf = ["{:.2f}".format(cf) for cf in coeffs]
+    # interpolated data
+    ax.plot(x1labels, (f(x1, *coeffs)), c=cat20_colours[i], linewidth=2, label=cf) #extrapolated labels/range
+    #ax.set_yscale("log")
+fig.autofmt_xdate()
+ax.set_title( title_str )
+ax.set_xlabel( "Data from https://github.com/CSSEGISandData/COVID-19" )
+ax.grid(linestyle='-', alpha=0.5) #, axis="y"
+days = mdates.DayLocator()
+ax.xaxis.set_major_locator(days)
+ax.legend(labelspacing=0.2, frameon=True)
+plt.tight_layout()
+pngfile="covid_pred.png"
+fig.savefig(pngfile, dpi=300)
+plt.show()
